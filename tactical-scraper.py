ae41f201ec742626354ced3d8cacbddff4d2dc33
FBI Tactical <71570618+FBITactical@users.noreply.github.com>
2020-09-20 04:11:11 +0000

    Update tactical-scraper.py

--- tactical-scraper.py
+++ tactical-scraper.py
@@ -4,6 +4,19 @@
 from phonenumbers import *
 import requests
 
+print()
+print("▄▄▄█████▓ ▄▄▄       ▄████▄  ▄▄▄█████▓ ██▓ ▄████▄   ▄▄▄       ██▓ ")   
+print("▓  ██▒ ▓▒▒████▄    ▒██▀ ▀█  ▓  ██▒ ▓▒▓██▒▒██▀ ▀█  ▒████▄    ▓██▒ ")   
+print("▒ ▓██░ ▒░▒██  ▀█▄  ▒▓█    ▄ ▒ ▓██░ ▒░▒██▒▒▓█    ▄ ▒██  ▀█▄  ▒██░ ")   
+print("░ ▓██▓ ░ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒░ ▓██▓ ░ ░██░▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░   ") 
+print("  ▒██▒ ░  ▓█   ▓██▒▒ ▓███▀ ░  ▒██▒ ░ ░██░▒ ▓███▀ ░ ▓█   ▓██▒░██████▒ ")
+print("  ▒ ░░    ▒▒   ▓▒█░░ ░▒ ▒  ░  ▒ ░░   ░▓  ░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░ ")
+print("    ░      ▒   ▒▒ ░  ░  ▒       ░     ▒ ░  ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░ ")
+print("  ░        ░   ▒   ░          ░       ▒ ░░          ░   ▒     ░ ░    ")
+print("               ░  ░░ ░                ░  ░ ░            ░  ░    ░  ░ ")
+print("                   ░                     ░                           ")
+print()
+
 phone_number = input("Enter a phone-number: ")
 response = requests.get("http://apilayer.net/api/validate?access_key=0934c36cd49141e24778e5dffa755e5b&number=" + phone_number + "&country_code=US")
 
