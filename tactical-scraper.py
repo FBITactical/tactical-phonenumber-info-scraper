ca8e3744deb65ca5edc441e61401291fab925911
FBI Tactical <71570618+FBITactical@users.noreply.github.com>
2020-09-20 03:37:11 +0000

    Add files via upload

---
+++ tactical-scraper.py
@@ -0,0 +1,10 @@
+#Coded by:fbitactical on Instagram.
+#A-Python2-Phonenumber-Information-Scraper
+
+from phonenumbers import *
+import requests
+
+phone_number = input("Enter a phone-number: ")
+response = requests.get("http://apilayer.net/api/validate?access_key=0934c36cd49141e24778e5dffa755e5b&number=" + phone_number + "&country_code=US")
+
+print(response.json())
